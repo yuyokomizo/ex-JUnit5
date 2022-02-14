668eec236e02af70d49c43fd0228dc4f216c27a9
yuyokomizo <yu.yokomizo@rakus-partners.co.jp>
2022-02-14 11:00:45 +0900

    ex02まで Please enter the commit message for your changes. Lines starting

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -1,7 +1,26 @@
 package junit.tutorial.ex01.e01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class StringUtilsTest {
 
+	@Test
+	void test1() {
+		String result1 = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa",result1,"aaa→aaa");
+	}
+	
+	@Test
+	void test2() {
+		String result2 = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world",result2,"HelloWorld→hello_world");
+	}
+	
+	@Test
+	void test3() {
+		String result3 = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit",result3,"practiceJunit→practice_junit");
+	}
 }
